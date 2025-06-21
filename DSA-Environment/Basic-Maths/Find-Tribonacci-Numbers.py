30f364b2298755c3d99c47971ec46fb27efbb8ec
Madhav P <madhavp2023@gmail.com>
2025-06-21 23:43:03 +0530

    Learning Phase-1 Completed

--- DSA-Environment/Basic-Maths/Find-Tribonacci-Numbers.py
+++ DSA-Environment/Basic-Maths/Find-Tribonacci-Numbers.py
@@ -1,1 +1,27 @@
-# Q17) Find the Tribonacci Numbers Using While Loop (Example :- 0,)
+# Q17) Find the Tribonacci Numbers Using While Loop  ? (Example :-  0 , 1 , 1 ,2 , 4 , 7 , 13)   --> My Logic 
+
+
+
+
+
+
+
+
+
+
+
+
+# Q17) Find the Tribonacci Numbers Using For - Loop  ?     --> My Logic 
+
+
+
+
+
+# Q17) Find the Tribonacci Numbers Using Do - While Loop ?    --> My Logic 
+
+
+
+
+
+
+# Q17) Find the Tribonacci Numbers Using Function ?   --> My Logic 
