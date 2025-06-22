f38251555c2fddfc6be6b1c0f77a16db65209170
Madhav P <madhavp2023@gmail.com>
2025-06-22 22:04:26 +0530

    Learning Phase-1 Completed

--- DSA-Environment/Basic-Maths/Find-Fibonacci-Numbers.py
+++ DSA-Environment/Basic-Maths/Find-Fibonacci-Numbers.py
@@ -1,19 +1,19 @@
 # Q16) Find the  Fibonacci Numbers using while Loop (Example :-   0,1,1,2,3,5,8.......) ?   --> My Logic 
 
-Number1 = 0
-Number2 = 1 
-Number3 = 0
-Limit = int(input("Set Your Limit :- "))
+# Number1 = 0
+# Number2 = 1 
+# Number3 = 0
+# Limit = int(input("Set Your Limit :- "))
 
-print(f"The Fibonacci Numbers are :- ")
-print(Number1)
-while(Number3<Limit):
-    Number1 = Number2
-    Number2 = Number3
-    Number3 = Number1 + Number2
-    if Number3>Limit:
-        break
-    print(Number3)
+# print(f"The Fibonacci Numbers are :- ")
+# print(Number1)
+# while(Number3<Limit):
+#     Number1 = Number2
+#     Number2 = Number3
+#     Number3 = Number1 + Number2
+#     if Number3>Limit:
+#         break
+#     print(Number3)
 
 
 
@@ -37,21 +37,21 @@ The Fibonacci Numbers are :-
 
 # Q16) Find the  Fibonacci Numbers using For - Loop  ?   --> My Logic 
 
-Number1 = 0 
-Number2 = 1 
-Number3 = 0 
-Limit = int(input("Set Your Limit :- "))
-i=0
+# Number1 = 0 
+# Number2 = 1 
+# Number3 = 0 
+# Limit = int(input("Set Your Limit :- "))
+# i=0
 
-print("The Fibonacci Numbers are :-  ")
-print(Number1)
-for i in range(1, Limit):
-    Number1 = Number2
-    Number2 = Number3
-    Number3 = Number1 + Number2
-    if Number3>Limit:
-        break
-    print(Number3)
+# print("The Fibonacci Numbers are :-  ")
+# print(Number1)
+# for i in range(1, Limit):
+#     Number1 = Number2
+#     Number2 = Number3
+#     Number3 = Number1 + Number2
+#     if Number3>Limit:
+#         break
+#     print(Number3)
 
 
 
@@ -77,27 +77,27 @@ The Fibonacci Numbers are :-
 
 # Q16) Find the  Fibonacci Numbers using Do - While Loop  ?   --> My Logic 
 
-Number1 = 0
-Number2 = 1
-Number3 = 0
-Limit = int(input("Set Your Limit :- "))
+# Number1 = 0
+# Number2 = 1
+# Number3 = 0
+# Limit = int(input("Set Your Limit :- "))
 
-print("The Fibonacci Numbers are :-  ")
-print(Number1)
+# print("The Fibonacci Numbers are :-  ")
+# print(Number1)
 
-if Limit<=0:
-    print(f"Invalid Limit :- {Limit}")
-else:
-  while True:
-    if Number3!=Limit:
-        Number1 = Number2
-        Number2 = Number3
-        Number3 = Number1 + Number3
-        if Number3>Limit:
-            break
-        print(Number3)
-    else:
-        break
+# if Limit<=0:
+#     print(f"Invalid Limit :- {Limit}")
+# else:
+#   while True:
+#     if Number3!=Limit:
+#         Number1 = Number2
+#         Number2 = Number3
+#         Number3 = Number1 + Number3
+#         if Number3>Limit:
+#             break
+#         print(Number3)
+#     else:
+#         break
 
 
 
@@ -125,22 +125,18 @@ The Fibonacci Numbers are :-
 # Q16) Find the  Fibonacci Numbers using Functions  ?   --> My Logic 
 
 def Print_Fibonacci():
-      Number1 = 0 
-      Number2 = 1 
-      Number3 = 0
-      Fibonacci_Numbers = []
+      Fibonacci_Numbers = [0, 1]
       Limit = int(input("Set Your Limit :- "))
-      Fibonacci_Numbers.append(Number1)
-
-      print("The Fibonacci Numbers are :-  ")
-      while Number3<Limit:
-          Number1 = Number2
-          Number2 = Number3
-          Number3 = Number1 + Number2
-          if Number3>Limit:
-              break
-          Fibonacci_Numbers.append(Number3)
-      return Fibonacci_Numbers
+      if Limit<=0:
+          print(f"Invalid Limit :- {Limit}")
+      else:
+          print("The Fibonacci Numbers are :-  ")
+          while True:
+               sum_of_2 = sum(Fibonacci_Numbers[-2:])
+               if sum_of_2>Limit:
+                        break
+               Fibonacci_Numbers.append(sum_of_2)
+          return Fibonacci_Numbers
        
 
 print(Print_Fibonacci())
@@ -150,8 +146,8 @@ print(Print_Fibonacci())
 
 Output :- 
 
-Set Your Limit :- 25
+Set Your Limit :- 20
 The Fibonacci Numbers are :-  
-[0, 1, 1, 2, 3, 5, 8, 13, 21]
+[0, 1, 1, 2, 3, 5, 8, 13]
 
 '''
